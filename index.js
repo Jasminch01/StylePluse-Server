c57bfed5d8973340af5c134547ab53b0222579ec
jasmin <jasminchakma895@gmail.com>
2023-10-19 14:33:29 +0600

    secure mongo user and pass

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config()
 const brands = require("./brands.json");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-//StylePluse
-// PPfl8cNPH2kihBUQ
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
-  "mongodb+srv://StylePluse:PPfl8cNPH2kihBUQ@cluster1.yh8qk3b.mongodb.net/?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.yh8qk3b.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -34,7 +34,7 @@ async function run() {
       const result = await products.toArray();
       res.send(result);
     });
-    
+
     //products
     app.get("/products", async (req, res) => {
       const products = productsCollection.find();
